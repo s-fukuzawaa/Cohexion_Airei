a2f2472aa87b8e1ccd4f9a0e557cf0e631d6796d
s-fukuzawaa <s-fukuzawaa@51S50023543.it.bsd405.org>
2018-10-03 17:14:57 -0700

    improvent smart player

--- Conhexion/src/PlayerSmart.java
+++ Conhexion/src/PlayerSmart.java
@@ -22,7 +22,7 @@ public class PlayerSmart implements Player
 					Board b= new Board(board);
 
 					b.setPlayer(new Location(i,j), player);
-					for(int n=0; n<2000; n++)//
+					for(int n=0; n<5000; n++)//
 					{
 						Player[] players = new Player[] { new PlayerRandom(), new PlayerRandom() };
 
